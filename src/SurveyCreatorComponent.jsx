2f21089ae7c5bb881251adae90f0ca6c62357b13
mafckz <mafckz@gmail.com>
2024-04-20 23:51:29 +0500

    Initial commit

---
+++ src/SurveyCreatorComponent.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { SurveyCreator, SurveyCreatorComponent } from "survey-creator-react";
+import "survey-core/survey.i18n.js";
+import "survey-creator-core/survey-creator-core.i18n.js";
+import { surveyJSON } from "./survey_json";
+import "survey-core/defaultV2.css";
+import "survey-creator-core/survey-creator-core.css";
+import "./index.css";
+
+function SurveyCreatorRenderComponent() {
+  const options = {
+    showLogicTab: true,
+  };
+  const creator = new SurveyCreator(options);
+  creator.JSON = surveyJSON;
+  return <SurveyCreatorComponent creator={creator} />;
+}
+
+export default SurveyCreatorRenderComponent;
